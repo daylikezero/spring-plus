a6ef1a16532d232e9710c7bdbaed11db1bc4f658
yrchoi <daylikezero@gmail.com>
2025-03-12 16:13:30 +0900

    refactor: 9. Spring Security

---
+++ src/main/java/org/example/expert/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package org.example.expert.config;
+
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+    // @EnableMethodSecurity => @Secured 사용 가능
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ) // stateless 인증
+                .addFilterBefore(jwtAuthenticationFilter, SecurityContextHolderAwareRequestFilter.class)
+                .formLogin(AbstractHttpConfigurer::disable) // 폼로그인 비활성화
+                .anonymous(AbstractHttpConfigurer::disable) // 익명사용자 권한 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable) // BasicAuthenticationFilter 비활성화
+                .logout(AbstractHttpConfigurer::disable) // 로그아웃 비활성화 -> 클라이언트가 JWT 삭제
+                .rememberMe(AbstractHttpConfigurer::disable) // RememberMeAuthenticationFilter 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(request -> request.getRequestURI().startsWith("/auth")).permitAll()
+                        .requestMatchers("/test").hasAuthority(UserRole.Authority.ADMIN)
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+}
